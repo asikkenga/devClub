229da08c3e23cc0a165985d1c86c701ef4a97f0d
asikkenga <a.sikkenga@gmail.com>
2017-06-28 17:24:53 -0500

    fixed mobile menu, fixed layout issue

--- js/devclubJS.js
+++ js/devclubJS.js
@@ -1,3 +1,20 @@
 
 //add new js functions here
 
+//navbar - collapse on click fix
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
+
+//navbar - collapse when clicking outside menu
+jQuery('html').bind('click', function(e) {
+    if(jQuery(e.target).closest('.navbar').length == 0) {
+        // click happened outside of .navbar, so hide
+        var opened = jQuery('.navbar-collapse').hasClass('collapse in');
+        if ( opened === true ) {
+            jQuery('.navbar-collapse').collapse('hide');
+        }
+    }
+});
